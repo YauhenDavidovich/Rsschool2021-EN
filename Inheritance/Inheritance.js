656f43ef81c22bcbf635a28f7c23d91bb612cea3
Yauhen Davidovich <davidovich336@gmail.com>
2021-05-19 21:17:03 +0300

    feat: add IntBuilder plus method

--- Inheritance/Inheritance.js
+++ Inheritance/Inheritance.js
@@ -2,11 +2,20 @@ function baseBuilder(value) {
     this.value = value
 }
 
-baseBuilder.prototype.get = function () { return this.value}
+baseBuilder.prototype.get = function () {
+    return this.value
+}
 
 
 class IntBuilder extends baseBuilder {
     constructor(value = 0) {
         super(value);
+    }
+
+    plus(...values) {
+        for (let n of values) {
+            this.value += n;
+        }
+        return this;
     }
 }

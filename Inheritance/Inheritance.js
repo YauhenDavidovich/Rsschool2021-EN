0a717bbc6fe352a4add252e159451f56015aa299
Yauhen Davidovich <davidovich336@gmail.com>
2021-05-19 21:11:52 +0300

    feat: create IntBuilder

--- Inheritance/Inheritance.js
+++ Inheritance/Inheritance.js
@@ -5,3 +5,8 @@ function baseBuilder(value) {
 baseBuilder.prototype.get = function () { return this.value}
 
 
+class IntBuilder extends baseBuilder {
+    constructor(value = 0) {
+        super(value);
+    }
+}

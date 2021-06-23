4a61dcb93188ec5579c41768b8695870991c6146
Yauhen Davidovich <davidovich336@gmail.com>
2021-06-23 13:18:16 +0300

    feat: add switch language function

--- virtual-keyboard/js/Keyboard.js
+++ virtual-keyboard/js/Keyboard.js
@@ -56,5 +56,43 @@ export default class Keyboard {
     const keyObj = this.keyButtons.find((key) => key.code === code);
     if (!keyObj) return; // check whether button is in button set
     this.output.focus();
+
+    if (type.match(/keydown|mousedown/)) {
+      if (type.match(/key/)) event.preventDefault();
+      keyObj.div.classList.add('active');
+
+      //switch language
+      if (code.match(/Control/)) this.ctrlKey = true;
+      if (code.match(/Alt/)) this.altKey = true;
+      if (code.match(/Control/) && this.altKey) this.switchLanguage();
+      if (code.match(/Alt/) && this.ctrlKey) this.switchLanguage();
+
+    } else if (event.type.match(/keyup|mouseup/)) {
+      keyObj.div.classList.remove('active');
+
+      if(code.match(/Control/)) this.ctrlKey = false;
+      if(code.match(/Alt/)) this.altKey = false;
+    }
+  }
+
+  switchLanguage = () => {
+    const langAb = Object.keys(language);
+    let lang = langAb.indexOf(this.container.dataset.language);
+    this.keyBase = lang + 1 < langAb.length ? language[langAb[lang += 1]]
+      : language[langAb[lang -= lang]];
+    this.container.dataset.language = langAb[lang];
+    storage.set('kbLang', langAb[lang])
+    this.keyButtons.forEach((button) => {
+      const keyObj = this.keyBase.find((key) => key.code === button.code);
+      if (!keyObj) return;
+      button.shift = keyObj.shift;
+      button.small = keyObj.small;
+      if (keyObj.shift && keyObj.shift.match(/[^a-zA-Zа-яА-ЯёЁ0-9]/g)) {
+        button.sub.innerHTML = keyObj.shift;
+      } else {
+        button.sub.innerHTML = '';
+      }
+      button.letter.innerHTML = keyObj.small;
+    });
   }
 }

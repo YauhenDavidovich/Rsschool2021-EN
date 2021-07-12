b0ba0cd5204ab6d794a1e3a2d78751a808111ea5
Yauhen Davidovich <davidovich336@gmail.com>
2021-07-12 10:30:28 +0200

    feat: add weather api script

---
+++ fancy-weather/src/js/view/api/getForecast.js
@@ -0,0 +1,19 @@
+import renderError from '../view/renderError.js';
+
+export default async function getForecast(loc) {
+  const API_TOKEN = '094d2f01c9172cdbf32222e4fee342c6';
+  const { latitude, longitude } = loc;  
+  
+  const URL = `https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&${longitude}&lang=eng&appid=${API_TOKEN}&units=metric`;
+
+  try {
+    const forecast = await fetch(proxyUrl + URL).then(res => res.json());
+    return forecast;
+  } catch (err) {
+    err.name = 'Forecast Error';
+    err.message =
+      'Forecast fetch error or just cors-anywhere.herokuapp.com unavailable again';
+    renderError(err);
+    throw new Error(`${err.name}(${err.code}): ${err.message}`);
+  }
+}

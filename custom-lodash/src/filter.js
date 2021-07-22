2b0c829ef552538cda7a9d4ae83c15280d235b43
Yauhen Davidovich <davidovich336@gmail.com>
2021-07-22 14:55:55 +0200

    fix: remove forbidden array.prototype

--- custom-lodash/src/filter.js
+++ custom-lodash/src/filter.js
@@ -2,8 +2,12 @@ function filter(arr, predicate) {
     if (!Array.isArray(arr)) {
         throw new TypeError(`${arr} is not an array`);
     }
-
-    return arr.filter((el, idx, arr) => predicate(el, idx, arr));
+    for (let elem of arr) {
+        if (predicate(elem)) {
+            return elem
+        }
+    }
+    return undefined
 }
 
 export default filter
